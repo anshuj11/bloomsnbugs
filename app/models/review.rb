10a0d7b7e808cf35bc6e2d398897a4d6fa184365
Anshuj <anshuj@gmail.com>
2019-08-29 05:59:57 -0700

    Adds comma in review.rb: Fixes compilation error

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-validates :listing_id, :user_id, :body  presence: true
+validates :listing_id, :user_id, :body,  presence: true
 
   belongs_to :user
   belongs_to :listing
