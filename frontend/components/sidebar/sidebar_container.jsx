f655d5c8e67e30228068a1ed82a2b1150022bd87
Anshuj <anshuj@gmail.com>
2019-10-05 08:27:40 -0700

    Fixes sidebar links

--- frontend/components/sidebar/sidebar_container.jsx
+++ frontend/components/sidebar/sidebar_container.jsx
@@ -7,19 +7,27 @@ const Sidebar = () => {
       <ul>
         <li className="SidebarItems">
           {" "}
-          <Link to="/about">About Us </Link>{" "}
+          <Link to="/about">
+            <div className="SidebarText">About Us</div>{" "}
+          </Link>{" "}
         </li>
         <li className="SidebarItems">
           {" "}
-          <Link to="/dresses">Dresses</Link>
+          <Link to="/dresses">
+            <div className="SidebarText">Dresses</div>
+          </Link>
         </li>
         <li className="SidebarItems">
           {" "}
-          <Link to="/accessories">Accessories</Link>
+          <Link to="/accessories">
+            <div className="SidebarText">Accessories</div>
+          </Link>
         </li>
         <li className="SidebarItems">
           {" "}
-          <a href="https://www.etsy.com/shop/BloomsNBugs#reviews">Reviews</a>
+          <a href="https://www.etsy.com/shop/BloomsNBugs#reviews">
+            <div className="SidebarText">Reviews</div>
+          </a>
         </li>
       </ul>
     </div>
